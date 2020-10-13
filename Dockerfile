e2e415cb1855f4a4f7c856c56c0a1f6dc52448df
Claudio Scheer <claudioscheer@protonmail.com>
2020-10-13 13:44:35 -0300

    Apache Hadoop setup script

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM ubuntu:latest
 
-RUN apt update
-RUN apt full-upgrade
+RUN apt-get update
+RUN apt-get upgrade -y
+
+COPY ./hadoop-setup.sh /home
+RUN chmod +x /home/hadoop-setup.sh
+RUN /home/hadoop-setup.sh
+RUN rm /home/hadoop-setup.sh
---
+++ hadoop-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get install openjdk-8-jdk -y
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> ~/.bashrc
+source ~/.bashrc
+
+apt-get install ssh pdsh -y
+
+wget https://downloads.apache.org/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz -O hadoop.tar.gz
+tar -xvf hadoop.tar.gz
+rm hadoop.tar.gz
+
+
