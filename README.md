7faa0fb8703ad94066b82ffab93dda64b939f55f
Claudio Scheer <claudioscheer@protonmail.com>
2020-10-22 18:31:46 -0300

    Update Hadoop install script

---
+++ README.md
@@ -0,0 +1,3 @@
+# How to install
+
+A guide on how to install Hadoop can be found [here](https://github.com/claudioscheer/claudioscheer.github.io/blob/master/posts/hadoop/install-hadoop-multi-node-cluster.md).
--- scripts/hadoop-base-setup.sh
+++ scripts/hadoop-base-setup.sh
@@ -3,6 +3,7 @@
 sudo apt-get install openjdk-8-jdk -y
 sudo apt-get install ssh pdsh neovim git openssh-server -y
 
+# Allow ssh localhost.
 ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
@@ -11,22 +12,18 @@ wget https://downloads.apache.org/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
 tar -xvf $HOME/hadoop.tar.gz -C $HOME
 rm $HOME/hadoop.tar.gz
 
-cat >> $HOME/.bashrc << EOF
+echo >> $HOME/.bashrc
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> $HOME/.bashrc
+echo 'export HADOOP_HOME=$HOME/hadoop-3.3.0' >> $HOME/.bashrc
+echo 'export HADOOP_INSTALL=$HADOOP_HOME' >> $HOME/.bashrc
+echo 'export HADOOP_MAPRED_HOME=$HADOOP_HOME' >> $HOME/.bashrc
+echo 'export HADOOP_COMMON_HOME=$HADOOP_HOME' >> $HOME/.bashrc
+echo 'export HADOOP_HDFS_HOME=$HADOOP_HOME' >> $HOME/.bashrc
+echo 'export HADOOP_YARN_HOME=$HADOOP_HOME' >> $HOME/.bashrc
+echo 'export HADOOP_CONF_DIR=$HADOOP_INSTALL/etc/hadoop' >> $HOME/.bashrc
+echo 'export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native' >> $HOME/.bashrc
+echo 'export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin' >> $HOME/.bashrc
+echo 'export PDSH_RCMD_TYPE=ssh' >> $HOME/.bashrc
+echo 'export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"' >> $HOME/.bashrc
 
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export HADOOP_HOME=$HOME/hadoop-3.3.0
-export HADOOP_INSTALL=$HADOOP_HOME
-export HADOOP_MAPRED_HOME=$HADOOP_HOME
-export HADOOP_COMMON_HOME=$HADOOP_HOME
-export HADOOP_HDFS_HOME=$HADOOP_HOME
-export YARN_HOME=$HADOOP_HOME
-export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
-export PDSH_RCMD_TYPE=ssh
-export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
-EOF
-
-cat >> $HOME/hadoop-3.3.0/etc/hadoop/hadoop-env.sh << EOF
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-EOF
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> $HOME/hadoop-3.3.0/etc/hadoop/hadoop-env.sh
