1271173ac81fefe8ed8b52645a042a725df28426
Claudio Scheer <claudioscheer@protonmail.com>
2020-10-21 22:37:58 -0300

    Setup a Hadoop base installation

--- scripts/hadoop-base-setup.sh
+++ scripts/hadoop-base-setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 sudo apt-get install openjdk-8-jdk -y
-sudo apt-get install ssh pdsh neovim -y
+sudo apt-get install ssh pdsh neovim git openssh-server -y
 
 ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 
-# wget https://downloads.apache.org/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz -O $HOME/hadoop.tar.gz
+wget https://downloads.apache.org/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz -O $HOME/hadoop.tar.gz
 tar -xvf $HOME/hadoop.tar.gz -C $HOME
 rm $HOME/hadoop.tar.gz
 
-cat >> $HOME/hadoop-3.3.0/etc/hadoop/hadoop-env.sh << EOF
+cat >> $HOME/~.bashrc << EOF
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export HADOOP_HOME=$HOME/hadoop-3.3.0
@@ -24,4 +24,10 @@ export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
 export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
 export PDSH_RCMD_TYPE=ssh
 export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
+EOF
+source $HOME/.bashrc
+
+cat >> $HOME/hadoop-3.3.0/etc/hadoop/hadoop-env.sh << EOF
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 EOF
