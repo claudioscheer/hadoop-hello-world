f0db8abd1e943eef635084782f889f1ccb58ccdf
Claudio Scheer <claudioscheer@protonmail.com>
2020-10-21 23:04:47 -0300

    Fix .bashrc path

--- scripts/hadoop-base-setup.sh
+++ scripts/hadoop-base-setup.sh
@@ -11,7 +11,7 @@ wget https://downloads.apache.org/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
 tar -xvf $HOME/hadoop.tar.gz -C $HOME
 rm $HOME/hadoop.tar.gz
 
-cat >> $HOME/~.bashrc << EOF
+cat >> $HOME/.bashrc << EOF
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export HADOOP_HOME=$HOME/hadoop-3.3.0
@@ -25,7 +25,6 @@ export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
 export PDSH_RCMD_TYPE=ssh
 export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
 EOF
-source $HOME/.bashrc
 
 cat >> $HOME/hadoop-3.3.0/etc/hadoop/hadoop-env.sh << EOF
 
