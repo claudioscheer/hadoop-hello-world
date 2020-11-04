c0cac28b04ed0195fdce342405a921c2857c9ef4
Claudio Scheer <claudioscheer@protonmail.com>
2020-11-04 19:07:14 -0300

    Update projects

--- projects/count-products-sold/src/main/java/CountProductsSold.java
+++ projects/count-products-sold/src/main/java/CountProductsSold.java
@@ -3,31 +3,34 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.MRJobConfig;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 
 public class CountProductsSold {
 
-    public static class SalesMapper extends Mapper<Object, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
+    public class SalesMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private Text country = new Text();
+        private IntWritable one = new IntWritable(1);
 
         @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String valueString = value.toString();
             String[] singleCountryData = valueString.split(",");
-
-            context.write(new Text(singleCountryData[7]), one);
+            country.set(singleCountryData[7]);
+            context.write(country, one);
         }
     }
 
-    public static class SalesReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public class SalesReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable frequency = new IntWritable();
 
         @Override
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
@@ -35,12 +38,14 @@ public class CountProductsSold {
             for (IntWritable val : values) {
                 frequencyForCountry += val.get();
             }
-            context.write(key, new IntWritable(frequencyForCountry));
+            frequency.set(frequencyForCountry);
+            context.write(key, frequency);
         }
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Configuration config = new Configuration();
+        config.set(TextOutputFormat.SEPARATOR, ",");
 
         Job job = Job.getInstance(config, "count products sold");
         job.setJarByClass(CountProductsSold.class);
@@ -49,14 +54,15 @@ public class CountProductsSold {
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
 
+        job.setCombinerClass(SalesReducer.class);
+
         job.setReducerClass(SalesReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-        job.setCombinerClass(SalesReducer.class);
-
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
--- projects/word-counting/src/main/java/WordCount.java
+++ projects/word-counting/src/main/java/WordCount.java
@@ -6,12 +6,9 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.MRJobConfig;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
@@ -20,8 +17,8 @@ import java.util.StringTokenizer;
 public class WordCount {
 
     // https://hadoop.apache.org/docs/current/api/org/apache/hadoop/mapreduce/Mapper.html
-    public static class TokenizerMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
+    public class TokenizerMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
         @Override
@@ -34,7 +31,7 @@ public class WordCount {
         }
     }
 
-    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
         @Override
@@ -50,15 +47,23 @@ public class WordCount {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Configuration config = new Configuration();
+
         Job job = Job.getInstance(config, "word count");
         job.setJarByClass(WordCount.class);
+
         job.setMapperClass(TokenizerMapper.class);
-        job.setReducerClass(IntSumReducer.class);
+
         job.setCombinerClass(IntSumReducer.class);
+
+        job.setReducerClass(IntSumReducer.class);
+        job.setNumReduceTasks(2);
+
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
         TextInputFormat.addInputPath(job, new Path(args[0]));
         TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
